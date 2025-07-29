a2758d63bcf79f51135329373b9faa0b037d63ae
Eniola Aderounmu <date1272@gmail.com>
2025-07-29 03:35:34 +0100

    done with hero and adding clerk

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
 
 const App = () => {
+
+  const isOwnerPath = useLocation().pathname.includes("owner") // To hide the navbar when logged in as a hotel owner 
+
   return (
     <div>
-      
+      {!isOwnerPath && <Navbar />}
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </div>
   )
 }
